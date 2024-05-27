bd3e41ef6ecb69285348bd73165f131f445a5599
jankoob <kontakt@jankoob.de>
2024-05-27 14:11:12 +0200

    Fix purged css in svg

--- src/transforms/css-purge-inline.js
+++ src/transforms/css-purge-inline.js
@@ -34,7 +34,7 @@ module.exports = async (content, outputPath) => {
       ],
       css: cssFiles,
       safelist: {
-        standard: [/^md/, /^lg/],
+        standard: [/^md/, /^lg/, /^code/, /^comment/],
         greedy: [/role$/, /where$/, /is$/, /youtube-embed$/, /vimeo-embed$/],
       },
       keyframes: true,
